b56e45920c237b0e929640eb7239ec429da95a43
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-05-27 12:15:56 +0530

    Day 31: Built first React component structure

---
+++ Day31/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import Header from "./Header";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <Header />
+
+      <h1>⚛️ My First React App</h1>
+
+      <p>
+        Learning React components 🚀
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
