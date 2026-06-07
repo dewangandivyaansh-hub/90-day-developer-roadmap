1d3bf69e9026ffcad3a554f251e94f8fdf2593bd
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-07 08:58:37 +0530

    Day 42: Implemented recent city search list

---
+++ Day42/WeatherCard.jsx
@@ -0,0 +1,35 @@
+function WeatherCard({ weather }) {
+
+    return (
+
+        <div className="weather-box">
+
+            <h2>{weather.name}</h2>
+
+            <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="Weather Icon"
+            />
+
+            <p>
+                🌡️ Temperature: {weather.main.temp}°C
+            </p>
+
+            <p>
+                ☁️ Condition: {weather.weather[0].main}
+            </p>
+
+            <p>
+                💧 Humidity: {weather.main.humidity}%
+            </p>
+
+            <p>
+                🌬️ Wind Speed: {weather.wind.speed} m/s
+            </p>
+
+        </div>
+
+    );
+}
+
+export default WeatherCard;
