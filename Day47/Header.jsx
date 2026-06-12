1f334624c899690add93fa4ee9282045a13a3dfb
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-12 09:13:38 +0530

    Day 47: Updated Hero Battle Arena header and game title

---
+++ Day47/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+
+    return (
+
+        <div>
+
+            <h1>
+                ⚔️ Hero Battle Arena
+            </h1>
+
+            <p>
+                React State Battle Simulator
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Header;
