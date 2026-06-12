544ecf014e761d03e53ca856c36719340b53560c
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-12 09:14:32 +0530

    Day 47: Connected battle arena components

---
+++ Day47/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Header from "./Header";
+import BattleCard from "./BattleCard";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <Header />
+
+      <BattleCard />
+
+    </div>
+
+  );
+
+}
+
+export default App;
