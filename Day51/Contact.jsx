4fd943c919b64a58f6f2ddcca0fbb98aa41b6732
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-16 14:50:03 +0530

    Day 51: Created Contact page component

---
+++ Day51/Contact.jsx
@@ -0,0 +1,19 @@
+function Contact() {
+
+    return (
+
+        <div>
+
+            <h1>📧 Contact</h1>
+
+            <p>
+                Your contact details here.
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Contact;
