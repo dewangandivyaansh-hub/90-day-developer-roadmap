2e4d3584cfbda0e6111fdff787672ed10b4f00c4
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-16 14:49:08 +0530

    Day 51: Created About page component

---
+++ Day51/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+
+    return (
+
+        <div>
+
+            <h1>👨‍💻 About</h1>
+
+            <p>
+                I am learning React and Web Development.
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default About;
