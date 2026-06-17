a51650cacd0eec75cbe0fccd7080865a278b186b
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-17 11:42:13 +0530

    Day 52: Added professional portfolio homepage

---
+++ Day52/Home.jsx
@@ -0,0 +1,29 @@
+function Home() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                👋 Hi, I'm Divyaansh
+            </h1>
+
+            <h2>
+                Frontend Developer
+            </h2>
+
+            <p>
+
+                Building projects with
+                HTML, CSS,
+                JavaScript and React.
+
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
