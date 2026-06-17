a32d053e27f955e9aaed4357bfb56e872e2c4f48
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-17 11:43:38 +0530

    Day 52: Added portfolio projects section

---
+++ Day52/Projects.jsx
@@ -0,0 +1,33 @@
+function Projects() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                Projects
+            </h1>
+
+            <ul>
+
+                <li>
+                    Weather App
+                </li>
+
+                <li>
+                    Hero Battle Arena
+                </li>
+
+                <li>
+                    React Counter App
+                </li>
+
+            </ul>
+
+        </div>
+
+    );
+
+}
+
+export default Projects;
