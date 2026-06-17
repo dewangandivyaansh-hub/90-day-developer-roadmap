48c135e17bf0ef4b5a6b80a240209503c7b84c06
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-17 11:45:46 +0530

    Day 52: Added contact information page

---
+++ Day52/Contact.jsx
@@ -0,0 +1,27 @@
+function Contact() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                Contact
+            </h1>
+
+            <p>
+                Email:
+                dewangandivyaansh@gmail.com
+            </p>
+
+            <p>
+                GitHub:
+                https://github.com/dewangandivyaansh-hub
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Contact;
