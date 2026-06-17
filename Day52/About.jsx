3ea7a70c999e17ec09a7aa1f8ab0c640ec6bb458
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-17 11:43:15 +0530

    Day 52: Updated About page content

---
+++ Day52/About.jsx
@@ -0,0 +1,32 @@
+function About() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                About Me
+            </h1>
+
+            <p>
+
+                I am currently learning
+                React and modern web
+                development.
+
+            </p>
+
+            <p>
+
+                My goal is to become
+                a full-stack developer.
+
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default About;
