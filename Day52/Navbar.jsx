d28573858fc06c87f899321c6af5ddb1ce4bf8dd
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-17 11:39:17 +0530

    Day 52: Created reusable Navbar component

---
+++ Day52/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+
+        <nav className="navbar">
+
+            <h2>
+                Divyaansh Portfolio
+            </h2>
+
+            <div>
+
+                <Link to="/">
+                    Home
+                </Link>
+
+                <Link to="/about">
+                    About
+                </Link>
+
+                <Link to="/projects">
+                    Projects
+                </Link>
+
+                <Link to="/contact">
+                    Contact
+                </Link>
+
+            </div>
+
+        </nav>
+
+    );
+
+}
+
+export default Navbar;
