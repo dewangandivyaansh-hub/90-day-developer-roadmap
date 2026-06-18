92a7d50bbe35b8fbf5573894bdcc7c98b1a416bf
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-18 11:44:29 +0530

    Day 53: Added skills section to homepage

---
+++ Day53/Home.jsx
@@ -0,0 +1,47 @@
+function Home() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                👋 Hi, I'm Divyaansh
+            </h1>
+
+            <h2>
+                Frontend Developer
+            </h2>
+
+            <p>
+
+                Building modern web applications
+                using HTML, CSS, JavaScript
+                and React.
+
+            </p>
+
+            <div className="skills">
+
+                <h3>Skills</h3>
+
+                <span>HTML</span>
+
+                <span>CSS</span>
+
+                <span>JavaScript</span>
+
+                <span>React</span>
+
+                <span>Git</span>
+
+                <span>GitHub</span>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
