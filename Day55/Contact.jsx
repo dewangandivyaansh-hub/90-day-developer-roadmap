c0f0679c7fa722ae2577fd267dbae7811de7d15f
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-06-20 11:15:23 +0530

    Day 55: Expanded contact page with professional links

---
+++ Day55/Contact.jsx
@@ -0,0 +1,41 @@
+function Contact() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                📧 Contact
+            </h1>
+
+            <p>
+                Email:
+            </p>
+
+            <p>
+                your@email.com
+            </p>
+
+            <p>
+                GitHub:
+            </p>
+
+            <p>
+                github.com/yourusername
+            </p>
+
+            <p>
+                LinkedIn:
+            </p>
+
+            <p>
+                linkedin.com/in/yourprofile
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Contact;
