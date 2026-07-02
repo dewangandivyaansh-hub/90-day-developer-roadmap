1253f47786b5614ef05d0e07ac2d5f3823a96e1c
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-07-02 10:42:47 +0530

    Day 66: Learn NumPy arrays and basic operations

---
+++ Day66/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+marks = np.array([85, 92, 78, 88, 95])
+
+print("Student Marks")
+
+print(marks)
+
+print()
+
+print("First Mark:")
+
+print(marks[0])
+
+print()
+
+print("Highest Mark:")
+
+print(np.max(marks))
+
+print()
+
+print("Lowest Mark:")
+
+print(np.min(marks))
+
+print()
+
+print("Average Mark:")
+
+print(np.mean(marks))
+
+print()
+
+print("Total Marks:")
+
+print(np.sum(marks))
